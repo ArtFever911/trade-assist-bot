74df6cbda45a77f4e09b4d6c3506e9df14ea98bb
artfever911 <ypleshynets@gmail.com>
2024-05-30 22:44:53 +0300

    feat: added server and endpoint to notify

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { TelegramBot } = require('./packages');
+const { TelegramBot, Server } = require('./packages');
 const unexpectedErrorHandler = (error) => {
     console.error(error);
     process.exit();
@@ -9,4 +9,6 @@ process.on('unhandledRejection', unexpectedErrorHandler);
 process.on('SIGTERM', () => {
     console.info('SIGTERM received');
 });
+
 TelegramBot.getInstance().init();
+Server.getInstance().init();
